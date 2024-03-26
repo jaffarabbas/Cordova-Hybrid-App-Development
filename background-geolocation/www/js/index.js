471945e904fda9460ba48255dec1a44c4694be30
jaffarabbas <gamaportal8@gmail.com>
2024-03-26 23:07:46 +0500

    iniriaize project

---
+++ background-geolocation/www/js/index.js
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+
+
+    BackgroundGeolocation.configure({
+        locationProvider: BackgroundGeolocation.ACTIVITY_PROVIDER,
+        desiredAccuracy: BackgroundGeolocation.HIGH_ACCURACY,
+        stationaryRadius: 50,
+        distanceFilter: 50,
+        notificationTitle: 'Background tracking',
+        notificationText: 'enabled',
+        debug: true,
+        interval: 10000,
+        fastestInterval: 5000,
+        activitiesInterval: 10000,
+      });
+
+        BackgroundGeolocation.start();
+        BackgroundGeolocation.on('location', function(location) {
+            console.log(location);
+        });
+
+        BackgroundGeolocation.on('stationary', function(stationaryLocation) {
+            console.log(stationaryLocation);
+        });
+
+        BackgroundGeolocation.startTask(function(taskKey) {
+            console.log(taskKey);
+            BackgroundGeolocation.start();
+            BackgroundGeolocation.on('location', function(location) {
+                console.log(location);
+            });
+
+            BackgroundGeolocation.on('stationary', function(stationaryLocation) {
+                console.log(stationaryLocation);
+            });
+        })
+}
